69249c5bc4baeb0f365a85eb870c601cd58a553e
rc5409 <63893231+rc5409@users.noreply.github.com>
2025-06-14 18:31:27 -0400

    Update main.py

--- app/main.py
+++ app/main.py
@@ -11,19 +11,6 @@ app = FastAPI(
 
 app.include_router(ai_router)
 
-
-### old ###
-# from fastapi import FastAPI
-# from app.routes.ai import router as ai_router
-
-# app = FastAPI(
-#     title="AI Backend Assessment",
-#     description="Prompt in, OpenAI answer out",
-#     version="1.0.0",
-# )
-
-# app.include_router(ai_router)
-
 ### test ###
 # app = FastAPI()
 
